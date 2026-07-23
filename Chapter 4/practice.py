07eccd93b12f5393d8110bbb3bec82aa763f09db
yadav-shikha <shikhayadavg991@gamil.com>
2026-07-23 21:15:21 +0530

    solved practice question of chapter 4

--- Chapter 4/practice.py
+++ Chapter 4/practice.py
@@ -11,12 +11,22 @@
 
 # print(fruits)
 # 1. Write a program to store 3 student in a list entered by the user.
-student = []
-stu1,stu2,stu3 = input('Enter student name ').split(' ')
-student.append(stu1)
-student.append(stu2)
-student.append(stu3)
-print(student)
+# student = []
+# stu1,stu2,stu3 = input('Enter student name ').split(' ')
+# student.append(stu1)
+# student.append(stu2)
+# student.append(stu3)
+# print(student)
 
 # 2. Write a program to accept marks of 6 students and display them in a sorted manner.
-# marks = 
+marks = (50,36,78,69,53)
+print(sorted(marks))
+
+# 3. Check that a tuple type cannot be changed in python.
+# marks[0] = 20
+# print(marks)
+
+# 5. Write a program to count the number of zeros in the following tuple:
+a = (7, 0, 8, 0, 0, 9)
+print(a.count(0))
+print(sum(a))
