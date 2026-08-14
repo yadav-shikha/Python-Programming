47f7d82bd6af72b15f8bbc9cbbe4066d5079c024
yadav-shikha <shikhayadavg991@gamil.com>
2026-08-14 06:43:33 +0530

    day 4 ..

---
+++ Turtle/04_index.py
@@ -0,0 +1,40 @@
+import turtle
+pen = turtle.Turtle()
+# background color
+screen = turtle.Screen()
+# screen.bgcolor('yellow')
+# shape
+# pen.shape('classic')
+# pen.forward(100)
+
+# speed()
+# pen.speed(1)
+
+# fill color
+# pen.fillcolor('red')
+# pen.begin_fill()
+# for i in range(5):
+#     pen.forward(150)
+#     pen.right(144)
+# pen.end_fill()
+# pen size
+# pen.width(5)
+# pen.pensize(5)
+
+# hideturtle
+# pen.hideturtle()
+
+# pen.home()
+# pen.clear()
+
+# random color drawing
+# import random
+# colors = ['red','green','yellow','blue','purple','orange','cyan']
+# for i in range(30):
+#     pen.pencolor(random.choice(colors))
+#     pen.forward(100)
+#     pen.right(170)
+
+# screen title
+screen.title('Shikha turtle program')
+turtle.done()
